daf223e312aa7c96c0b417d5ac50b922b728058a
notfork-h <hacene@unforkable.co>
2025-04-29 16:53:33 +0200

    Refactor message enum in protocol module to add codec indices for various message types.

--- substrate/client/network/src/protocol/message.rs
+++ substrate/client/network/src/protocol/message.rs
@@ -79,40 +79,49 @@ pub mod generic {
 	#[derive(Debug, PartialEq, Eq, Clone, Encode, Decode)]
 	pub enum Message<Header, Hash, Number, Extrinsic> {
 		/// Status packet.
+		#[codec(index = 0)]
 		Status(Status<Hash, Number>),
 		/// Block request.
+		#[codec(index = 1)]
 		BlockRequest(BlockRequest<Hash, Number>),
 		/// Block response.
+		#[codec(index = 2)]
 		BlockResponse(BlockResponse<Header, Hash, Extrinsic>),
 		/// Block announce.
+		#[codec(index = 3)]
 		BlockAnnounce(BlockAnnounce<Header>),
 		/// Consensus protocol message.
-		// NOTE: index is incremented by 1 due to transaction-related
-		// message that was removed
-		#[codec(index = 6)]
+		#[codec(index = 6)] // gap volontaire ici
 		Consensus(ConsensusMessage),
 		/// Remote method call request.
+		#[codec(index = 7)]
 		RemoteCallRequest(RemoteCallRequest<Hash>),
 		/// Remote method call response.
+		#[codec(index = 8)]
 		RemoteCallResponse(RemoteCallResponse),
 		/// Remote storage read request.
+		#[codec(index = 9)]
 		RemoteReadRequest(RemoteReadRequest<Hash>),
 		/// Remote storage read response.
+		#[codec(index = 10)]
 		RemoteReadResponse(RemoteReadResponse),
 		/// Remote header request.
+		#[codec(index = 11)]
 		RemoteHeaderRequest(RemoteHeaderRequest<Number>),
 		/// Remote header response.
+		#[codec(index = 12)]
 		RemoteHeaderResponse(RemoteHeaderResponse<Header>),
 		/// Remote changes request.
+		#[codec(index = 13)]
 		RemoteChangesRequest(RemoteChangesRequest<Hash>),
 		/// Remote changes response.
+		#[codec(index = 14)]
 		RemoteChangesResponse(RemoteChangesResponse<Number, Hash>),
 		/// Remote child storage read request.
+		#[codec(index = 15)]
 		RemoteReadChildRequest(RemoteReadChildRequest<Hash>),
 		/// Batch of consensus protocol messages.
-		// NOTE: index is incremented by 2 due to finality proof related
-		// messages that were removed.
-		#[codec(index = 17)]
+		#[codec(index = 17)] // autre gap volontaire ici
 		ConsensusBatch(Vec<ConsensusMessage>),
 	}
 
