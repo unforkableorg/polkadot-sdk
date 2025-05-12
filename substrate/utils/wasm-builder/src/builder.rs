18a1b20890bacd43b7e730ba0b7321af01520301
notfork-h <hacene@unforkable.co>
2025-05-12 15:04:41 +0200

    Remove no_default_features option from WasmBuilder to simplify build configuration and enhance clarity.

--- substrate/utils/wasm-builder/src/builder.rs
+++ substrate/utils/wasm-builder/src/builder.rs
@@ -161,7 +161,6 @@ impl WasmBuilder {
 			.with_current_project()
 			.import_memory()
 			.export_heap_base()
-			.no_default_features(true) // 👈 empêche les features std de remonter
 			.build();
 	}
 
